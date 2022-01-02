37a3882a28f137b62fbe6a2b2bf80b49a83c85c8
Rustam Amirov <r.amirov@dsserv.ru>
2022-01-02 03:34:09 +0300

    second step

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import {Command} from 'commander';
 
 export default () => {
     const program = new Command();
-    program.version('1.0.0').description('Compares two configuration files and shows a difference.');
+    program
+        .version('1.0.0')
+        .description('Compares two configuration files and shows a difference.')
+        .option('-f, --format [type]', 'output format')
+        .arguments('<filePath1> <filePath2>')
+        .action((filePath1, filePath2) => {});
     program.parse();
 };
